5cc15e2ada1554c819a4669048b7106f7e636a35
Patrick Schmidt <ps.patti1998@gmail.com>
2023-03-09 19:26:21 +0100

    webview_flutter updated 3.0.2 -> 4.0.6

--- lib/ui/screens/setting/imprint/imprint_view.dart
+++ lib/ui/screens/setting/imprint/imprint_view.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class ImprintPage extends StatelessWidget {
+class ImprintPage extends HookWidget {
   const ImprintPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Legal Content'),
-        ),
-        body: const WebView(
-          initialUrl:
-              'https://www.iubenda.com/privacy-policy/19183925/full-legal',
-          javascriptMode: JavascriptMode.unrestricted,
-        ),
-      );
+  Widget build(BuildContext context) {
+    var webViewController = useState(WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(
+          'https://www.iubenda.com/privacy-policy/19183925/full-legal')));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Legal Content'),
+      ),
+      body: WebViewWidget(
+        controller: webViewController.value,
+      ),
+    );
+  }
 }
