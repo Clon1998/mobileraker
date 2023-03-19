1ee1cfae3220a9fb1ffe015c454f37c4f6ac03ab
Patrick Schmidt <ps.patti1998@gmail.com>
2023-03-19 15:23:13 +0100

    Imprint view now also offers option to open it externally!

--- lib/ui/screens/setting/imprint/imprint_view.dart
+++ lib/ui/screens/setting/imprint/imprint_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ImprintPage extends HookWidget {
@@ -7,14 +8,29 @@ class ImprintPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    var imprint =
+        Uri.parse('https://www.iubenda.com/privacy-policy/19183925/full-legal');
+
     var webViewController = useState(WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse(
-          'https://www.iubenda.com/privacy-policy/19183925/full-legal')));
+      ..loadRequest(imprint));
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Legal Content'),
+        actions: [
+          IconButton(
+              tooltip: 'Open in Browser',
+              onPressed: () async {
+                if (await canLaunchUrl(imprint)) {
+                  await launchUrl(imprint,
+                      mode: LaunchMode.externalApplication);
+                } else {
+                  throw 'Could not launch $imprint';
+                }
+              },
+              icon: const Icon(Icons.open_in_browser))
+        ],
       ),
       body: WebViewWidget(
         controller: webViewController.value,
