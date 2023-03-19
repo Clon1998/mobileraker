abfbd175ac349495ae46755b1ae8e3a0ff45fc6c
Patrick Schmidt <ps.patti1998@gmail.com>
2023-03-19 16:03:44 +0100

    Improved Imprint view

--- lib/ui/screens/setting/imprint/imprint_view.dart
+++ lib/ui/screens/setting/imprint/imprint_view.dart
@@ -1,5 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:progress_indicators/progress_indicators.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -11,9 +14,17 @@ class ImprintPage extends HookWidget {
     var imprint =
         Uri.parse('https://www.iubenda.com/privacy-policy/19183925/full-legal');
 
-    var webViewController = useState(WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(imprint));
+    var imprintLoaded = useState(false);
+
+    WebViewController webViewController = useMemoized<WebViewController>(() {
+      var webVC = WebViewController();
+      webVC.clearCache().then((value) async {
+        await webVC.setJavaScriptMode(JavaScriptMode.unrestricted);
+        await webVC.loadRequest(imprint);
+        imprintLoaded.value = true;
+      });
+      return webVC;
+    });
 
     return Scaffold(
       appBar: AppBar(
@@ -21,19 +32,42 @@ class ImprintPage extends HookWidget {
         actions: [
           IconButton(
               tooltip: 'Open in Browser',
-              onPressed: () async {
-                if (await canLaunchUrl(imprint)) {
-                  await launchUrl(imprint,
-                      mode: LaunchMode.externalApplication);
-                } else {
-                  throw 'Could not launch $imprint';
-                }
-              },
+              onPressed: imprintLoaded.value
+                  ? () async {
+                      if (await canLaunchUrl(imprint)) {
+                        await launchUrl(imprint,
+                            mode: LaunchMode.externalApplication);
+                      } else {
+                        throw 'Could not launch $imprint';
+                      }
+                    }
+                  : null,
               icon: const Icon(Icons.open_in_browser))
         ],
       ),
-      body: WebViewWidget(
-        controller: webViewController.value,
+      body: AnimatedSwitcher(
+        transitionBuilder: (child, anim) => SizeTransition(
+          sizeFactor: anim,
+          child: child,
+        ),
+        duration: kThemeAnimationDuration,
+        switchInCurve: Curves.easeIn,
+        switchOutCurve: Curves.easeOut,
+        child: (imprintLoaded.value)
+            ? WebViewWidget(
+                controller: webViewController,
+              )
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SpinKitFoldingCube(
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 32),
+                      child: FadingText('${tr('general.loading')} ...'))
+                ],
+              ),
       ),
     );
   }
