38b6fac741b950657ad2d007fe56373c345927d8
Patrick Schmidt <ps.patti1998@gmail.com>
2023-05-20 11:37:13 +0100

    Fixed webcam card was shown if no webcam is configured!

--- lib/ui/screens/dashboard/components/webcams/cam_card.dart
+++ lib/ui/screens/dashboard/components/webcams/cam_card.dart
@@ -2,9 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:mobileraker/data/data_source/json_rpc_client.dart';
 import 'package:mobileraker/data/enums/webcam_service_type.dart';
-import 'package:mobileraker/logger.dart';
 import 'package:mobileraker/ui/components/webcam/webcam_mjpeg.dart';
 import 'package:mobileraker/ui/screens/dashboard/components/webcams/cam_card_controller.dart';
 import 'package:mobileraker/util/extensions/async_ext.dart';
@@ -19,63 +17,75 @@ class CamCard extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     var watch = ref.watch(camCardControllerProvider);
 
-    return Card(
-      child: Column(
-        children: [
-          ListTile(
-            leading: const Icon(
-              FlutterIcons.webcam_mco,
-            ),
-            title: const Text('pages.dashboard.general.cam_card.webcam').tr(),
-            trailing: const _Trailing(),
-          ),
-          Container(
-            padding: const EdgeInsets.fromLTRB(8, 8, 8, 10),
-            child: AnimatedSwitcher(
-              switchInCurve: Curves.easeInOutBack,
-              duration: const Duration(milliseconds: 400),
-              transitionBuilder: (child, anim) => SizeTransition(
-                  sizeFactor: anim,
-                  child: FadeTransition(
-                    opacity: anim,
-                    child: child,
-                  )),
-              child: watch.when(
-                  data: (data) => data.allCams.isEmpty
-                      ? const SizedBox.shrink()
-                      : Center(
-                          key: UniqueKey(), child: _CamCardData(data: data)),
-                  error: (e, s) => Center(
-                        child: Column(
-                          key: UniqueKey(),
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Icon(Icons.error_outline),
-                            const SizedBox(
-                              height: 30,
+    bool showCard = (watch.valueOrFullNull?.activeCam != null &&
+            watch.valueOrFullNull?.allCams.isNotEmpty == true) ||
+        watch.hasError;
+
+    return AnimatedSwitcher(
+      switchInCurve: Curves.easeInOutBack,
+      duration: const Duration(milliseconds: 400),
+      transitionBuilder: (child, anim) => SizeTransition(
+          sizeFactor: anim,
+          child: FadeTransition(
+            opacity: anim,
+            child: child,
+          )),
+      child: showCard
+          ? Card(
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: const Icon(
+                      FlutterIcons.webcam_mco,
+                    ),
+                    title: const Text('pages.dashboard.general.cam_card.webcam')
+                        .tr(),
+                    trailing: const _Trailing(),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(8, 8, 8, 10),
+                    child: watch.when(
+                        data: (data) =>
+                            data.allCams.isEmpty || data.activeCam == null
+                                ? const SizedBox.shrink()
+                                : Center(
+                                    key: UniqueKey(),
+                                    child: _CamCardData(data: data)),
+                        error: (e, s) => Center(
+                              child: Column(
+                                key: UniqueKey(),
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  const Icon(Icons.error_outline),
+                                  const SizedBox(
+                                    height: 30,
+                                  ),
+                                  Text(e.toString(),
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .error)),
+                                  TextButton.icon(
+                                      onPressed: () => ref
+                                          .read(camCardControllerProvider
+                                              .notifier)
+                                          .onRetry(),
+                                      icon: const Icon(
+                                          Icons.restart_alt_outlined),
+                                      label: const Text('general.retry').tr())
+                                ],
+                              ),
                             ),
-                            Text(e.toString(),
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    color:
-                                        Theme.of(context).colorScheme.error)),
-                            TextButton.icon(
-                                onPressed: () => ref
-                                    .read(camCardControllerProvider.notifier)
-                                    .onRetry(),
-                                icon: const Icon(Icons.restart_alt_outlined),
-                                label: const Text('general.retry').tr())
-                          ],
-                        ),
-                      ),
-                  loading: () => Container(
-                      alignment: Alignment.center,
-                      child: const LinearProgressIndicator())),
-            ),
-          ),
-        ],
-      ),
+                        loading: () => Container(
+                            alignment: Alignment.center,
+                            child: const LinearProgressIndicator())),
+                  ),
+                ],
+              ),
+            )
+          : const SizedBox.shrink(),
     );
   }
 }
@@ -89,12 +99,14 @@ class _Trailing extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     var camState = ref.watch(camCardControllerProvider).valueOrFullNull;
 
-    if (camState == null || camState.allCams.length < 2) {
+    if (camState == null ||
+        camState.allCams.length < 2 ||
+        camState.activeCam == null) {
       return const SizedBox.shrink();
     }
 
     return DropdownButton(
-        value: camState.activeCam.uuid,
+        value: camState.activeCam!.uuid,
         onChanged:
             ref.read(camCardControllerProvider.notifier).onSelectedChange,
         items: camState.allCams.map((e) {
@@ -117,7 +129,7 @@ class _CamCardData extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     var activeCam = data.activeCam;
 
-    switch (activeCam.service) {
+    switch (activeCam!.service) {
       case WebcamServiceType.mjpegStreamer:
       case WebcamServiceType.mjpegStreamerAdaptive:
       case WebcamServiceType.uv4lMjpeg:
