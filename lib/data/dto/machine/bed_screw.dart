c27c08f7d3c2f6e64acd58f380024de3a2d1c174
Patrick Schmidt <ps.patti1998@gmail.com>
2023-05-30 23:15:23 +0100

    Added bed_screw and manual_probe

---
+++ lib/data/dto/machine/bed_screw.dart
@@ -0,0 +1,33 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'bed_screw.freezed.dart';
+part 'bed_screw.g.dart';
+
+// "bed_screws": {
+//   "state": "adjust", // nullable, either adjust or fine (For fineAdjust mode???)
+//   "is_active": true,
+//   "accepted_screws": 0,
+//   "current_screw": 0
+// }
+
+enum BedScrewMode { adjust, fine }
+
+@freezed
+class BedScrew with _$BedScrew {
+  const factory BedScrew({
+    @JsonKey(name: 'is_active') @Default(false) bool isActive,
+    BedScrewMode? state,
+    @JsonKey(name: 'accepted_screws') @Default(0) int acceptedScrews,
+    @JsonKey(name: 'current_screw') @Default(0) int currentScrew,
+  }) = _BedScrew;
+
+  factory BedScrew.fromJson(Map<String, dynamic> json) =>
+      _$BedScrewFromJson(json);
+
+  factory BedScrew.partialUpdate(
+      BedScrew? current, Map<String, dynamic> partialJson) {
+    BedScrew old = current ?? const BedScrew();
+    var mergedJson = {...old.toJson(), ...partialJson};
+    return BedScrew.fromJson(mergedJson);
+  }
+}
