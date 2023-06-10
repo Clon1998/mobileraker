fb6504bca983071260da7b54d8d49c02f6b3450f
Patrick Schmidt <ps.patti1998@gmail.com>
2023-06-10 01:42:49 +0100

    Added style to IconElevatedButton

--- lib/ui/components/IconElevatedButton.dart
+++ lib/ui/components/IconElevatedButton.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 class SquareElevatedIconButton extends StatelessWidget {
   const SquareElevatedIconButton(
-      {Key? key, this.onPressed, this.margin, this.child})
+      {Key? key, this.onPressed, this.style, this.margin, this.child})
       : super(key: key);
 
   final VoidCallback? onPressed;
 
   final EdgeInsetsGeometry? margin;
+
+  final ButtonStyle? style;
 
   final Widget? child;
 
@@ -17,7 +19,8 @@ class SquareElevatedIconButton extends StatelessWidget {
         margin: margin,
         child: ElevatedButton(
             key: key,
-            style: ElevatedButton.styleFrom(minimumSize: const Size.square(40)),
+            style: ElevatedButton.styleFrom(minimumSize: const Size.square(40))
+                .merge(style),
             onPressed: onPressed,
             child: child));
   }
