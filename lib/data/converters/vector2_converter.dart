90bb1159d99d250ac7e7c36d038af2ee1223be97
Patrick Schmidt <ps.patti1998@gmail.com>
2023-06-27 10:04:57 +0200

    Fixed exclude_object parsing

--- lib/data/converters/vector2_converter.dart
+++ lib/data/converters/vector2_converter.dart
@@ -6,12 +6,12 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:vector_math/vector_math.dart';
 
-class Vector2Converter extends JsonConverter<Vector2, List<dynamic>> {
+class Vector2Converter extends JsonConverter<Vector2, List<dynamic>?> {
   const Vector2Converter();
 
   @override
-  Vector2 fromJson(List<dynamic> json) {
-    if (json.isEmpty) return Vector2.zero();
+  Vector2 fromJson(List<dynamic>? json) {
+    if (json == null || json.isEmpty) return Vector2.zero();
 
     var list = json.map((e) => (e as num).toDouble()).toList();
     return Vector2.array(list);
