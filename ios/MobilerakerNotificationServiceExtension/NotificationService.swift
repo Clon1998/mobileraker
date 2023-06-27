f94d1d88bea5a3812da02fe4485e6a0968b533eb
Patrick Schmidt <ps.patti1998@gmail.com>
2023-06-27 22:11:11 +0200

    ReAdded NSE

---
+++ ios/MobilerakerNotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationService.swift
+//  MobilerakerNotificationServiceExtension
+//
+//  Created by Patrick Schmidt on 27.06.23.
+//
+
+import UserNotifications
+import awesome_notifications_fcm
+
+@available(iOS 10.0, *)
+class NotificationService: DartAwesomeServiceExtension {
+
+}
