40f0765bd43227e49e99c312e9bad84fcba76cde
Patrick Schmidt <ps.patti1998@gmail.com>
2023-11-02 12:25:53 +0100

    Add dev lol

--- lib/ui/components/drawer/nav_drawer_view.dart
+++ lib/ui/components/drawer/nav_drawer_view.dart
@@ -9,7 +9,6 @@ import 'package:common/service/selected_machine_service.dart';
 import 'package:common/service/ui/theme_service.dart';
 import 'package:common/util/extensions/async_ext.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
@@ -91,8 +90,8 @@ class NavigationDrawerWidget extends ConsumerWidget {
                       icon: Icons.history,
                       routeName: '/changelog',
                     ),
-                    if (kDebugMode)
-                      const _DrawerItem(
+                    // if (kDebugMode)
+                    const _DrawerItem(
                         text: 'DEV',
                         icon: FlutterIcons.build_mdi,
                         routeName: '/dev',
