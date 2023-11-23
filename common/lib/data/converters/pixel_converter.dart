ef02c835350420606c1e4ab53eed3f6b35c0666d
Patrick Schmidt <ps.patti1998@gmail.com>
2023-11-23 15:06:52 +0100

    #287 - Fixed parsing for legacy neopixel config

--- common/lib/data/converters/pixel_converter.dart
+++ common/lib/data/converters/pixel_converter.dart
@@ -7,17 +7,34 @@ import 'package:json_annotation/json_annotation.dart';
 
 import '../dto/machine/leds/led.dart';
 
-class PixelConverter extends JsonConverter<Pixel, List<dynamic>> {
+class PixelConverter extends JsonConverter<Pixel, dynamic> {
   const PixelConverter();
 
   @override
-  Pixel fromJson(List<dynamic> json) {
+  Pixel fromJson(dynamic json) {
+    if (json is Map) {
+      return fromMap((json).cast<String, dynamic>());
+    } else if (json is List) {
+      return fromList(json);
+    } else {
+      throw ArgumentError('PixelConverter: Unknown type: ${json.runtimeType}');
+    }
+  }
+
+  Pixel fromList(List<dynamic> json) {
     if (json.isEmpty) return const Pixel();
 
     var list = json.map((e) => (e as num).toDouble()).toList();
     return Pixel.fromList(list);
   }
 
+  Pixel fromMap(Map<String, dynamic> json) {
+    if (json.isEmpty) return const Pixel();
+
+    var map = json.map((key, value) => MapEntry(key.toUpperCase(), (value as num).toDouble()));
+    return Pixel.fromMap(map);
+  }
+
   @override
-  List<dynamic> toJson(Pixel object) => object.asList().toList(growable: false);
+  dynamic toJson(Pixel object) => object.legacy ? object.asMap() : object.asList().toList(growable: false);
 }
