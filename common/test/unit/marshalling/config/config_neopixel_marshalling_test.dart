513522f55b76a03e8d182c35b80768f75051e79a
Patrick Schmidt <ps.patti1998@gmail.com>
2023-11-23 15:19:41 +0100

    #287 - Added parsing tests

--- common/test/unit/marshalling/config/config_neopixel_marshalling_test.dart
+++ common/test/unit/marshalling/config/config_neopixel_marshalling_test.dart
@@ -61,4 +61,31 @@ void main() {
     expect(configNeopixel.initialBlue, 0);
     expect(configNeopixel.initialWhite, 0);
   });
+
+  test('ConfigNeopixel.fromJson(), color_order as String (Klipper < 11.x)', () {
+    const neopixelStr = '''
+    {
+      "pin": "EXP1_6",
+      "chain_count": 3,
+      "color_order": "RGB",
+      "initial_red": 1,
+      "initial_green": 1,
+      "initial_blue": 1,
+      "initial_white": 0
+    }
+    ''';
+    var neopixelJson = jsonDecode(neopixelStr);
+
+    var configNeopixel = ConfigNeopixel.fromJson("fysetc_mini12864", neopixelJson);
+
+    expect(configNeopixel, isNotNull);
+    expect(configNeopixel.name, "fysetc_mini12864");
+    expect(configNeopixel.pin, "EXP1_6");
+    expect(configNeopixel.colorOrder, "RGB");
+    expect(configNeopixel.chainCount, 3);
+    expect(configNeopixel.initialRed, 1.0);
+    expect(configNeopixel.initialGreen, 1.0);
+    expect(configNeopixel.initialBlue, 1.0);
+    expect(configNeopixel.initialWhite, 0);
+  });
 }
