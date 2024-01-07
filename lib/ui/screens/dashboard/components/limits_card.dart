fab7ed4f1529f16f9a7dd45e55e06caa7f25fccc
Patrick Schmidt <ps.patti1998@gmail.com>
2024-01-07 14:54:58 +0100

    Updated Copyright Notice

--- lib/ui/screens/dashboard/components/limits_card.dart
+++ lib/ui/screens/dashboard/components/limits_card.dart
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023. Patrick Schmidt.
+ * Copyright (c) 2023-2024. Patrick Schmidt.
  * All rights reserved.
  */
 import 'package:common/service/moonraker/klippy_service.dart';
