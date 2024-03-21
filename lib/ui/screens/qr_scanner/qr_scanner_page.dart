5fb2fb18c33a4706b0b5f60043aff27f3d69f264
Patrick Schmidt <ps.patti1998@gmail.com>
2024-03-21 17:11:14 +0100

    Linter fix unnecessary_cast

--- lib/ui/screens/qr_scanner/qr_scanner_page.dart
+++ lib/ui/screens/qr_scanner/qr_scanner_page.dart
@@ -32,7 +32,7 @@ class _QrScannerPageState extends ConsumerState<QrScannerPage> {
             icon: ValueListenableBuilder(
               valueListenable: cameraController.torchState,
               builder: (context, state, child) {
-                switch (state as TorchState) {
+                switch (state) {
                   case TorchState.off:
                     return const Icon(Icons.flash_off, color: Colors.grey);
                   case TorchState.on:
@@ -48,7 +48,7 @@ class _QrScannerPageState extends ConsumerState<QrScannerPage> {
             icon: ValueListenableBuilder(
               valueListenable: cameraController.cameraFacingState,
               builder: (context, state, child) {
-                switch (state as CameraFacing) {
+                switch (state) {
                   case CameraFacing.front:
                     return const Icon(Icons.camera_front);
                   case CameraFacing.back:
