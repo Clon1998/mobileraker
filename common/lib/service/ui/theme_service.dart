e934629a51f187a0e6f8e09f2f3983194cbe975b
Patrick Schmidt <ps.patti1998@gmail.com>
2024-03-23 15:43:29 +0100

    Fixed some refs

--- common/lib/service/ui/theme_service.dart
+++ common/lib/service/ui/theme_service.dart
@@ -4,7 +4,6 @@
  */
 
 import 'dart:async';
-import 'dart:math';
 
 import 'package:common/service/selected_machine_service.dart';
 import 'package:common/service/setting_service.dart';
@@ -77,13 +76,12 @@ class ThemeService {
   /// Selects the system's default theme pack based on user preferences or falls
   /// back to the first theme pack in the list if the stored selection is out of range.
   void selectSystemThemePack() {
-    var selIndex = _settingService.readInt(AppSettingKeys.themePack);
-    int themeIndex = selIndex;
+    var settingIndex = _settingService.readInt(AppSettingKeys.themePack);
+    int themeIndex = settingIndex.clamp(0, themePacks.length - 1);
 
-    logger.i('Theme selected: $selIndex, available theme len: ${themePacks.length}');
-    if (selIndex > themePacks.length - 1) themeIndex = 0;
-
-    var mode = ThemeMode.values[min(_settingService.readInt(AppSettingKeys.themeMode), themePacks.length - 1)];
+    logger.i('Theme selected: $settingIndex, available theme len: ${themePacks.length}');
+    var modeIndex = _settingService.readInt(AppSettingKeys.themeMode).clamp(0, 2);
+    var mode = ThemeMode.values[modeIndex];
     activeTheme = ThemeModel(themePacks[themeIndex], mode);
   }
 
