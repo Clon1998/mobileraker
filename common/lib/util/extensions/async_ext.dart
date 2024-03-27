4413075e7fc88a8e0df640f8d11ef8068a7baf32
Patrick Schmidt <ps.patti1998@gmail.com>
2024-03-27 23:19:23 +0100

    Fixed a bug in selectAs that did not turn AsyncError(isLoading:true) into a transformed loading error

--- common/lib/util/extensions/async_ext.dart
+++ common/lib/util/extensions/async_ext.dart
@@ -19,12 +19,20 @@ extension AlwaysAliveAsyncDataSelector<Input> on ProviderListenable<AsyncValue<I
       /// We explicitly differentiate between data, error, and loading states while considering the previous value.
       /// If the previous value existed, we apply the selector function to transform the data, preventing issues with
       /// empty/new error/loading AsyncValue instances that lack values and might lead to unexpected behavior.
-
       return value.map(
         data: (data) => data.whenData(selector),
-        error: (error) => error.hasValue
-            ? AsyncData<Output>(selector(value.value as Input)).toError(error.error, error.stackTrace)
-            : AsyncValue<Output>.error(error.error, error.stackTrace),
+        error: (error) {
+          var asyncValue = error.hasValue
+              ? AsyncData<Output>(selector(value.value as Input)).toError(error.error, error.stackTrace)
+              : AsyncValue<Output>.error(error.error, error.stackTrace).toLoading();
+
+          // Also handle the case where an error is loading
+          if (error.isLoading) {
+            return AsyncValue<Output>.loading().copyWithPrevious(asyncValue, isRefresh: true);
+          }
+
+          return asyncValue;
+        },
         loading: (loading) {
           if (!loading.hasValue) {
             return AsyncValue<Output>.loading();
