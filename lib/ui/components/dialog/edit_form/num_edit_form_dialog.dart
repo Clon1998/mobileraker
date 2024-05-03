0ead8fffd92c61362afd10f7e4afb0e6f0089f05
Patrick Schmidt <ps.patti1998@gmail.com>
2024-05-03 20:26:05 +0200

    Fixed ruler style in NumEditDialog

--- lib/ui/components/dialog/edit_form/num_edit_form_dialog.dart
+++ lib/ui/components/dialog/edit_form/num_edit_form_dialog.dart
@@ -306,6 +306,7 @@ class _RangeEditSlider extends HookWidget {
             showSecondaryRulers: false,
             showLabel: true,
             labelOffset: 5,
+            textStyle: themeData.textTheme.bodySmall,
           ),
         ),
       ],
