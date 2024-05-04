836247811c3790e1e6faa85b6148db8a79821446
Patrick Schmidt <ps.patti1998@gmail.com>
2024-05-05 00:49:38 +0200

    #340 - Added z_thermal_adjust.dart object

---
+++ common/lib/data/dto/machine/z_thermal_adjust.dart
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2023-2024. Patrick Schmidt.
+ * All rights reserved.
+ */
+
+import 'package:common/data/dto/machine/sensor_mixin.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'z_thermal_adjust.freezed.dart';
+part 'z_thermal_adjust.g.dart';
+
+// {
+// 'temperature': self.smoothed_temp,
+// 'measured_min_temp': round(self.measured_min, 2),
+// 'measured_max_temp': round(self.measured_max, 2),
+// 'current_z_adjust': self.z_adjust_mm,
+// 'z_adjust_ref_temperature': self.ref_temperature,
+// 'enabled': self.adjust_enable
+// }
+
+@freezed
+class ZThermalAdjust with _$ZThermalAdjust, SensorMixin {
+  const ZThermalAdjust._();
+
+  @JsonSerializable(fieldRename: FieldRename.snake)
+  const factory ZThermalAdjust({
+    @Default(false) bool enabled,
+    @Default(0) double temperature,
+    @Default(0) double measuredMinTemp,
+    @Default(0) double measuredMaxTemp,
+    @Default(0) double currentZAdjust,
+    @Default(0) double zAdjustRefTemperature,
+    @JsonKey(name: 'temperatures') List<double>? temperatureHistory,
+    required DateTime lastHistory,
+  }) = _ZThermalAdjust;
+
+  factory ZThermalAdjust.fromJson(Map<String, dynamic> json) => _$ZThermalAdjustFromJson(json);
+
+  factory ZThermalAdjust.partialUpdate(ZThermalAdjust? current, Map<String, dynamic> partialJson) {
+    if (current == null) return ZThermalAdjust.fromJson(partialJson);
+
+    var mergedJson = {...current.toJson(), ...partialJson};
+    return ZThermalAdjust.fromJson(mergedJson);
+  }
+
+  @override
+  String get name => 'z_thermal_adjust';
+}
