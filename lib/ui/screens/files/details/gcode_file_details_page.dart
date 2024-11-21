3bea6280317e6f020b72709c2211f6e301ee0d47
Patrick Schmidt <ps.patti1998@gmail.com>
2024-11-21 22:56:56 +0100

    fix: Missing import

--- lib/ui/screens/files/details/gcode_file_details_page.dart
+++ lib/ui/screens/files/details/gcode_file_details_page.dart
@@ -35,6 +35,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mobileraker_pro/service/ui/pro_sheet_type.dart';
 import 'package:mobileraker_pro/spoolman/dto/get_spool.dart';
