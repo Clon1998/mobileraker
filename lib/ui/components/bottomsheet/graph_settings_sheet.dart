4ef0cf0f70787c889519c7e37e01cfdeb38b6ed0
Patrick Schmidt <ps.patti1998@gmail.com>
2025-01-26 20:15:06 +0100

    feat: i18n for graph setting sheet

--- lib/ui/components/bottomsheet/graph_settings_sheet.dart
+++ lib/ui/components/bottomsheet/graph_settings_sheet.dart
@@ -6,6 +6,7 @@
 import 'package:common/service/moonraker/temperature_store_service.dart';
 import 'package:common/service/setting_service.dart';
 import 'package:common/util/misc.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_icons/flutter_icons.dart';
@@ -25,9 +26,9 @@ class GraphSettingsSheet extends ConsumerWidget {
       child: ListTile(
         visualDensity: VisualDensity.compact,
         title: Text(
-          'Graph Settings',
+          'bottom_sheets.temp_chart_settings.title',
           style: Theme.of(context).textTheme.headlineSmall,
-        ),
+        ).tr(),
       ),
     );
 
@@ -102,6 +103,7 @@ class _OptionsList extends ConsumerWidget {
                           selected: active,
                           elevation: 2,
                           label: Text('Temp'),
+                          labelStyle: themeData.textTheme.bodySmall,
                           selectedColor: color.$2,
                           side: BorderSide(color: color.$1),
                           // backgroundColor: color.$2,
@@ -125,6 +127,7 @@ class _OptionsList extends ConsumerWidget {
                           selected: active,
                           elevation: 2,
                           label: Text('Target'),
+                          labelStyle: themeData.textTheme.bodySmall,
                           selectedColor: color.$2,
                           side: BorderSide(color: color.$2),
                           // backgroundColor: color.$2,
