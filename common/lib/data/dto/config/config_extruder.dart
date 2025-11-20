4cac89d6ea9bfa69900b99b08687a2146af13f4f
Patrick Schmidt <ps.patti1998@gmail.com>
2025-11-20 13:10:58 +0100

    fix: extruder config parsing error

--- common/lib/data/dto/config/config_extruder.dart
+++ common/lib/data/dto/config/config_extruder.dart
@@ -20,8 +20,8 @@ class ConfigExtruder with _$ConfigExtruder {
     required double maxTemp,
     required double maxPower,
     required double filamentDiameter,
-    required double maxExtrudeOnlyVelocity, // mm/s
-    required double maxExtrudeOnlyAccel, // mm/s^2
+    required double? maxExtrudeOnlyVelocity, // mm/s
+    required double? maxExtrudeOnlyAccel, // mm/s^2
   }) = _ConfigExtruder;
 
   factory ConfigExtruder.fromJson(String name, Map<String, dynamic> json) =>
