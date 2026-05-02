5c94b07cafdad9db37fb3c409eef769d2c0ae44a
Patrick Schmidt <ps.patti1998@gmail.com>
2026-05-02 17:13:52 +0200

    refactor(announcement_service): Reworked service to use purer riverpod provider approach

--- common/lib/service/moonraker/announcement_service.dart
+++ common/lib/service/moonraker/announcement_service.dart
@@ -3,102 +3,67 @@
  * All rights reserved.
  */
 
-import 'dart:async';
-
-import 'package:common/data/dto/jrpc/rpc_response.dart';
+import 'package:common/data/dto/announcement/announcement_entry.dart';
 import 'package:common/exceptions/mobileraker_exception.dart';
+import 'package:common/network/jrpc_client_provider.dart';
 import 'package:common/network/json_rpc_client.dart';
 import 'package:common/util/logger.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-import '../../data/dto/announcement/announcement_entry.dart';
-import '../../network/jrpc_client_provider.dart';
 
 part 'announcement_service.g.dart';
 
 @riverpod
-AnnouncementService announcementService(Ref ref, String machineUUID) {
-  return AnnouncementService(ref, machineUUID);
-}
+class Announcement extends _$Announcement {
+  JsonRpcClient get _jrpcClient => ref.read(jrpcClientProvider(machineUUID));
 
-@riverpod
-Stream<List<AnnouncementEntry>> announcement(Ref ref, String machineUUID) {
-  return ref.watch(announcementServiceProvider(machineUUID)).announcementNotificationStream;
-}
+  @override
+  Future<List<AnnouncementEntry>> build(String machineUUID) async {
+    ref.listen(jrpcMethodEventProvider(machineUUID, 'notify_announcement_update'), (_, next) {
+      if (!next.hasValue) return;
+      final raw = List<Map<String, dynamic>>.from(next.value!['params'] as List);
+      state = AsyncData(_parseList(raw));
+    });
+    ref.listen(jrpcMethodEventProvider(machineUUID, 'notify_announcement_dismissed'), (_, next) {
+      if (next.isLoading || next.hasError) return;
+      ref.invalidateSelf();
+    });
+    ref.listen(jrpcMethodEventProvider(machineUUID, 'notify_announcement_wake'), (_, next) {
+      if (next.isLoading || next.hasError) return;
+      ref.invalidateSelf();
+    });
 
-/// The AnnouncementService handles different notifications/announcements from feed api.
-/// For more information check out
-/// 1. https://moonraker.readthedocs.io/en/latest/web_api/#announcement-apis
-class AnnouncementService {
-  AnnouncementService(Ref ref, String machineUUID) : _jRpcClient = ref.watch(jrpcClientProvider(machineUUID)) {
-    ref.onDispose(dispose);
-    _jRpcClient.addMethodListener(_onNotifyAnnouncementUpdate, 'notify_announcement_update');
-    _jRpcClient.addMethodListener(_onNotifyAnnouncementDismissed, 'notify_announcement_dismissed');
-    _jRpcClient.addMethodListener(_onNotifyAnnouncementWake, 'notify_announcement_wake');
+    return _fetchList();
   }
 
-  final StreamController<List<AnnouncementEntry>> _announcementsStreamCtrler = StreamController();
-
-  Stream<List<AnnouncementEntry>> get announcementNotificationStream =>
-      _announcementsStreamCtrler.stream;
-
-  final JsonRpcClient _jRpcClient;
-
-  Future<List<AnnouncementEntry>> listAnnouncements([bool includeDismissed = false]) async {
-    talker.info('List Announcements request...');
-
+  Future<List<AnnouncementEntry>> _fetchList([bool includeDismissed = false]) async {
+    talker.info('[AnnouncementNotifier($machineUUID)] Fetching announcements...');
     try {
-      RpcResponse rpcResponse = await _jRpcClient.sendJRpcMethod('server.announcements.list',
-          params: {'include_dismissed': includeDismissed});
-
-      List<Map<String, dynamic>> entries = rpcResponse.result['entries'];
-
-      return _parseAnnouncementsList(entries);
+      final resp = await _jrpcClient.sendJRpcMethod(
+        'server.announcements.list',
+        params: {'include_dismissed': includeDismissed},
+      );
+      return _parseList(List<Map<String, dynamic>>.from(resp.result['entries'] as List));
     } on JRpcError catch (e) {
       throw MobilerakerException('Unable to fetch announcement list: $e');
     }
   }
 
   Future<String> dismissAnnouncement(String entryId, [int? wakeTime]) async {
-    talker.info('Trying to dismiss announcement `$entryId`');
-
+    talker.info('[AnnouncementNotifier($machineUUID)] Dismissing announcement $entryId...');
     try {
-      RpcResponse rpcResponse = await _jRpcClient.sendJRpcMethod('server.announcements.list',
-          params: {'entry_id': entryId, 'wake_time': wakeTime});
-
-      String respEntryId = rpcResponse.result['entry_id'];
-      return respEntryId;
+      final resp = await _jrpcClient.sendJRpcMethod(
+        'server.announcements.dismiss',
+        params: {
+          'entry_id': entryId,
+          if (wakeTime != null) 'wake_time': wakeTime,
+        },
+      );
+      return resp.result['entry_id'] as String;
     } on JRpcError catch (e) {
       throw MobilerakerException('Unable to dismiss announcement $entryId. Err: $e');
     }
   }
 
-  _onNotifyAnnouncementUpdate(Map<String, dynamic> rawMessage) {
-    List<Map<String, dynamic>> rawEntries = rawMessage['params'];
-    List<AnnouncementEntry> entries = _parseAnnouncementsList(rawEntries);
-    _announcementsStreamCtrler.add(entries);
-  }
-
-  _onNotifyAnnouncementDismissed(Map<String, dynamic> rawMessage) {
-    talker.info('Announcement dismissed event!!!');
-    listAnnouncements().then(_announcementsStreamCtrler.add);
-  }
-
-  _onNotifyAnnouncementWake(Map<String, dynamic> rawMessage) {
-    talker.info('Announcement wake event!!!');
-    listAnnouncements().then(_announcementsStreamCtrler.add);
-  }
-
-  List<AnnouncementEntry> _parseAnnouncementsList(List<Map<String, dynamic>> entries) {
-    return entries.map((e) => AnnouncementEntry.parse(e)).toList();
-  }
-
-  dispose() {
-    _jRpcClient.removeMethodListener(_onNotifyAnnouncementUpdate, 'notify_announcement_update');
-    _jRpcClient.removeMethodListener(_onNotifyAnnouncementDismissed, 'notify_announcement_dismissed');
-    _jRpcClient.removeMethodListener(_onNotifyAnnouncementWake, 'notify_announcement_wake');
-
-    _announcementsStreamCtrler.close();
-  }
+  List<AnnouncementEntry> _parseList(List<Map<String, dynamic>> entries) =>
+      entries.map((e) => AnnouncementEntry.parse(e)).toList();
 }
